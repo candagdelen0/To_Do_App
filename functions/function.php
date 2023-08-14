5e60c4674d18e2025f15036be558303c2e4ec716
candagdelen0 <candagdelen0@gmail.com>
2023-08-14 18:16:28 +0300

    Fonksiyonlar | Güvenlik Fonksiyonu

--- functions/function.php
+++ functions/function.php
@@ -2,6 +2,11 @@
     include "connection.php";
 
     class System {
-
+        public function safety($data) {
+            $data = trim($data);
+            $data = stripcslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
     }
 ?>
