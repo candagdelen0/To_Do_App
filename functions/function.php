04a03ed597c1d2e4971ba979c425cb23af4f3089
candagdelen0 <candagdelen0@gmail.com>
2023-08-15 15:32:45 +0300

    Ana Ekrana Günleri Çekme Fonksiyonu

--- functions/function.php
+++ functions/function.php
@@ -16,5 +16,44 @@
                 return $sonuc = $sorgu->get_result();
             }
         }
+
+        function haftagetir($vt) {
+            $sonuc = $this->genelsorgu($vt, "SELECT * FROM haftalik",1);
+            while ($dizi = $sonuc->FETCH_ASSOC()):
+                $gor = $this->genelsorgu($vt, 'SELECT * FROM tasks WHERE gunid ='.$dizi["id"].'',1);
+                $gunbak = $gor->num_rows; 
+                if ($gunbak == 0) {
+                    $renk = '#00BFFF;';
+                    $bord = "info";
+                    $icon = '<i class="fa-regular fa-calendar" style="color: #009dff;"></i>';
+                } else {
+                    $renk = '#1E90FF;';
+                    $bord = "primary";
+                    $icon = '<i class="fa-solid fa-calendar" style="color: #11255f;"></i>';
+                }
+
+                echo '<div class="col-md-3 mt-4">  
+                    <a href="" style="text-decoration: none;">          
+                        <div class="card border-' . $bord . ' m-1 col-md-12 p-2" > 
+                            <div class="card-body text-secondary"> 
+                                <p class="card-text">
+                                    <span style="font-size: 48px; color:' . $renk . '">'.$icon.'</span>
+                                    <span style="font-size: 24px;" class="ms-2"">' . $dizi["ad"] . '</span>';
+                                    if ($gunbak != 0): 
+                                        echo '<kbd style="float: right;">'. $gunbak .'</kbd>';
+                                    endif;
+                                echo '</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>';
+            endwhile;
+        }
+
+
+
+
+
+
     }
 ?>
