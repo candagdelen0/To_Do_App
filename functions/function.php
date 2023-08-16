47f32493e59c34c6288a51a3d1978c929aa44a61
candagdelen0 <candagdelen0@gmail.com>
2023-08-16 13:50:36 +0300

    İç Ekran | Ana form fonksiyonu

--- functions/function.php
+++ functions/function.php
@@ -66,6 +66,40 @@
             }
         }
 
+        public function detayGetir($vt) {
+          
+            echo '<div class="row">
+                <div class="border-bottom mt-2">';
+                   
+                echo '</div>
+                <div class="col-md-10 mx-auto mt-4">
+                    <form method="post">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="New Task" name="task" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <input type="submit" value="Add" class="btn btn-outline-primary" name="submit">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-12 border-bottom mt-2">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th colspan="1">#</th>
+                                <th colspan="3">Task</th>
+                                <th colspan="1">Status</th>
+                                <th colspan="1">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
+                            
+                        echo '</tbody>
+                    </table>
+                </div>
+            </div>';
+        }
+
 
 
 
