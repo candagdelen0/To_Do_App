24aa3f6bd2e1bae1ec9b4c12ff607d207ded4d9b
candagdelen0 <candagdelen0@gmail.com>
2023-08-16 13:45:38 +0300

    Uygulama Ana Ekran | id bilgileri editleme

--- functions/function.php
+++ functions/function.php
@@ -17,11 +17,11 @@
             }
         }
 
-        function haftagetir($vt) {
-            $id = $_GET["id"];
+        public function haftagetir($vt) {
+            $userid = $_GET["id"];
             $sonuc = $this->genelsorgu($vt, "SELECT * FROM haftalik",1);
             while ($dizi = $sonuc->FETCH_ASSOC()):
-                $gor = $this->genelsorgu($vt, 'SELECT * FROM tasks WHERE gunid ='.$dizi["id"].'',1);
+                $gor = $this->genelsorgu($vt, 'SELECT * FROM tasks WHERE gunid ='.$dizi["id"].' AND userid ='.$userid.'',1);
                 $gunbak = $gor->num_rows; 
                 if ($gunbak == 0) {
                     $renk = '#00BFFF;';
@@ -32,9 +32,8 @@
                     $bord = "primary";
                     $icon = '<i class="fa-solid fa-calendar" style="color: #11255f;"></i>';
                 }
-
                 echo '<div class="col-md-3 mt-4">  
-                    <a href="gundetay.php?id='.$id.'&gunid='.$dizi["id"].'" style="text-decoration: none;">               
+                    <a href="gundetay.php?id='.$userid.'&gunid='.$dizi["id"].'" style="text-decoration: none;">          
                         <div class="card border-' . $bord . ' m-1 col-md-12 p-2" > 
                             <div class="card-body text-secondary"> 
                                 <p class="card-text">
