e882df8fca5af44697e8f0a7e0de14d9a25422ea
candagdelen0 <candagdelen0@gmail.com>
2023-08-18 14:54:04 +0300

    İç Ekran | Görev Düzenleme Ana Form

---
+++ editTask.php
@@ -0,0 +1,19 @@
+<div class="container my-3">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="bg-primary mt-3 p-3 fs-3 text-center">Görevi Düzenle</div>
+            <div class="p-4" style="background-color: #F0F8FF;">
+                <form method="post">
+                    <div class="mb-3">
+                        <label for="task">Görev Tanımı</label>
+                        <textarea name="taskname" class="form-control">Task Name</textarea>
+                    </div>
+                    <div class="mb-5">
+                        <input type="submit" class="btn btn-primary float-end ps-5 pe-5" name="submit" value="Güncelle">
+                        <input type="submit" name="geri" class="btn btn-danger float-start ps-5 pe-5" value="İptal">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
