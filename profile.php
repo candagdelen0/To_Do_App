25ee9a51e4bcfc5ca891bed4dabca6f3d5b9d007
candagdelen0 <candagdelen0@gmail.com>
2023-08-20 13:12:06 +0300

    Bilgilerim | Ana form

---
+++ profile.php
@@ -0,0 +1,26 @@
+<div class="container my-3">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="bg-primary mt-3 p-3 fs-3 text-center">Profil Bilgilerim</div>
+            <div class="p-4" style="background-color: #F0F8FF;">
+                <form method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="username">Kullanıcı Adı</label>
+                        <input type="text" name="username" class="form-control"  required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email">Email Adresi</label>
+                        <input type="email" name="email" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" class="btn btn-primary mt-3 form-control" value="Güncelle" name="submit">
+                    </div>
+                </form>
+                <div class="pb-3">
+                    <a href="" class="float-start text-info" style="text-decoration: none;">Parolayı Değiştir</a>
+                    <a href="" class="float-end text-danger" style="text-decoration: none;">İptal</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
