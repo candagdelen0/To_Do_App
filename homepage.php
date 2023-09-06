735c0dddaffba8d164c6d03e346c9c20aff97993
candagdelen0 <candagdelen0@gmail.com>
2023-09-06 16:30:35 +0300

    Anasayfa | cookie düzenlemesi

--- homepage.php
+++ homepage.php
@@ -1,64 +1,15 @@
 <?php 
-  session_start();
   include "_header.php"; 
-  if (!$_SESSION['Kullanici']) {
+  require "functions/function.php";
+  $sistem = new System;
+ 
+  if (!$_COOKIE['user']) {
     header("Location:index.php");
   }
-?>
-<style>
-    .dropbtn {
-        background-color: #0B5ED7;
-        color: white;
-        padding: 12px;
-        font-size: 16px;
-        border: #0B5ED7;
-        cursor: pointer;
-        border-radius: 10px;
-    }
-
-    .dropdown {
-      position: relative;
-      display: inline-block;
-    }
-
-    .dropdown-content {
-      display: none;
-      position: absolute;
-      background-color: #f9f9f9;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-    }
-
-    .dropdown-content a {
-      color: black;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-    }
-
-    .dropdown-content a:hover {background-color: #f1f1f1}
-
-    .dropdown:hover .dropdown-content {
-      display: block;
-    }
-
-    .dropdown:hover .dropbtn {
-      background-color: white;
-      color: black;
-    }
-
-    a {
-        text-decoration: none;
-    }
-</style>
-
-<?php 
-    require "functions/function.php";
-    $sistem = new System;
-    $id = $_GET["id"];
-    $diz = $sistem->genelsorgu($db, "SELECT * FROM user WHERE id= $id",1);
-    while ($dizi = $diz->FETCH_ASSOC()):
+  
+  $id = $_GET["id"];
+  $diz = $sistem->genelsorgu($db, "SELECT * FROM user WHERE id= $id",1);
+  while ($dizi = $diz->FETCH_ASSOC()):
 ?>
 <nav class="navbar navbar-expand-lg bg-info">
     <div class="container">
