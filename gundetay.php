5f985efae1a2a499b72c480a02d17af4732b2815
candagdelen0 <candagdelen0@gmail.com>
2023-09-06 16:34:03 +0300

    İç sayfa | cookie düzenlemesi

--- gundetay.php
+++ gundetay.php
@@ -1,76 +1,29 @@
 <?php 
-  session_start();
   include "_header.php"; 
-  if (!$_SESSION['Kullanici']) {
+  require "functions/function.php";
+  if (!$_COOKIE['user']) {
     header("Location:index.php");
   }
-?>
-<style>
-    .dropbtn {
-        background-color: #0B5ED7;
-        color: white;
-        padding: 12px;
-        font-size: 16px;
-        border: #0B5ED7;
-        cursor: pointer;
-        border-radius: 10px;
-    }
-
-    .dropdown {
-      position: relative;
-      display: inline-block;
-    }
-
-    .dropdown-content {
-      display: none;
-      position: absolute;
-      background-color: #f9f9f9;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-    }
-
-    .dropdown-content a {
-      color: black;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-    }
-
-    .dropdown-content a:hover {background-color: #f1f1f1}
-
-    .dropdown:hover .dropdown-content {
-      display: block;
-    }
-
-    .dropdown:hover .dropbtn {
-      background-color: white;
-      color: black;
-    }
-
-    a {
-        text-decoration: none;
-    }
-
-    #left-arrow:hover{
-      background-color: #6495ED;
-    }
-</style>
-
-<?php 
-  require "functions/function.php";
+  
   $sistem = new System;
   $id = $_GET["id"];
   $diz = $sistem->genelsorgu($db, "SELECT * FROM user WHERE id= $id",1);
   while ($dizi = $diz->FETCH_ASSOC()):
 ?>
+<style>
+    #left-arrow:hover{
+      background-color: #6495ED;
+    }
+</style>
+<link rel="stylesheet" href="dropdown.css">
 <nav class="navbar navbar-expand-lg bg-info">
   <div class="container">
     <a href="homepage.php?id=<?php echo $id; ?>" class="navbar-brand"><i class="fa-solid fa-arrow-left me-2" style="color: white;"></i> <b>To Do App</b></a>
     <div class="navbar-nav dropdown me-2">
-        <button class="dropbtn nav-item"><?php echo $_SESSION['Kullanici']; ?></button>
+        <button class="dropbtn nav-item"><?php echo $_COOKIE['user']; ?></button>
           <div class="dropdown-content">
               <a href="profile.php?id=<?php echo $id; ?>"><i class="fa-solid fa-id-card me-2" style="color: #000000;"></i> Bilgilerim</a>
+              <a href="report.php?id=<?php echo $id; ?>"><i class="fa-regular fa-circle-question" style="color: #000000;"></i> Durum Raporu</a>
               <a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket me-2" style="color: #000000;"></i> Çıkış Yap</a>
           </div>
     </div> 
