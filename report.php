442b80ce48cd51692ec9da3aa24c367b17a781fd
candagdelen0 <candagdelen0@gmail.com>
2023-09-07 18:51:03 +0300

    Durum Bilgisi | Navbar

--- report.php
+++ report.php
@@ -4,5 +4,20 @@
     $sistem = new System;
     $id = $_GET["id"];
     $diz = $sistem->genelsorgu($db, "SELECT * FROM user WHERE id= $id",1);
-    
+    while ($dizi = $diz->FETCH_ASSOC()):
 ?>
+<link rel="stylesheet" href="dropdown.css">
+<nav class="navbar navbar-expand-lg bg-info">
+<div class="container">
+    <a href="homepage.php?id=<?php echo $id; ?>" class="navbar-brand"><i class="fa-solid fa-arrow-left me-2" style="color: white;"></i> <b>To Do App</b></a>
+    <div class="navbar-nav dropdown">
+        <button class="dropbtn nav-item"><?php echo $_COOKIE['user']; ?></button>
+        <div class="dropdown-content">
+            <a href="profile.php?id=<?php echo $id; ?>"><i class="fa-solid fa-id-card me-2" style="color: #000000;"></i> Bilgilerim</a>
+            <a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket me-2" style="color: #000000;"></i> Çıkış Yap</a>
+        </div>
+    </div> 
+</div>
+</nav>
+<?php endwhile; ?>
+
